83c02a41ccb32d24671c27df9af85744839d87a2
David Allen <david.allen@columnzero.com>
2023-10-07 01:36:39 -0400

    Acutally solve day 4 (key: "fully contains")

--- src/day04/d4.clj
+++ src/day04/d4.clj
@@ -6,10 +6,20 @@
 
 (defrecord ^:private Assignment [left right])
 
+(defn format-range [set]
+  (let [plot (map #(if (contains? set %) % ".") (range 0 10))]
+    (format "%s  %s-%s\n" (string/join plot) (apply min set) (apply max set))))
+
+(defn print-assignment [assignment]
+  (str (format-range (:left assignment))
+       (format-range (:right assignment))))
+
+(defn format-assignments [assignments] (map print-assignment assignments))
+
 (defn parse-range-set [range-str]
   (let [range-str-pair (string/split range-str #"-")
         range-pair (map #(Integer/parseInt %) range-str-pair)
-        range (apply range range-pair)]
+        range (range (first range-pair) (inc (second range-pair)))]
     (into #{} range)))
 
 (defn parse-assignment [line]
@@ -24,11 +34,13 @@
        (string/split-lines)
        (map parse-assignment)))
 
-(defn is-overlap [assignment]
-  (seq (set/intersection (:left assignment) (:right assignment))))
+(defn is-fully-contained [assignment]
+  (let [l (:left assignment)
+        r (:right assignment)]
+    (or (set/subset? l r) (set/subset? r l))))
 
 (defn num-overlaps [assignments]
-  (count (filter is-overlap assignments)))
+  (count (filter is-fully-contained assignments)))
 
 (defn part1 [filename]
   (as-> filename it
